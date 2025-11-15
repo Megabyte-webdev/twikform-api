280aa0655204c37080863f6610fd7155e98e6505
Afolabi Mubarak <126519914+Megabyte-webdev@users.noreply.github.com>
2025-11-15 22:15:57 +0100

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: ["https://twikfoundation.org", "https://twikfoundation.vercel.app"], // adjust to your frontend
+    origin: ["https://www.twikfoundation.org", "https://twikfoundation.org", "https://twikfoundation.vercel.app"], // adjust to your frontend
     credentials: true,
   })
 );
